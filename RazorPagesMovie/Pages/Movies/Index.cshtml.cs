a79383c3847bd9c3a2f18d3065002776591c2f10
pszczesz <pszczesz@poczta.onet.pl>
2018-10-31 16:19:28 +0100

    kol1

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -19,6 +20,9 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
