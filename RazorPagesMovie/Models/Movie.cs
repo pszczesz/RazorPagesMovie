752eac3b1bef30fc515182a51ea3d11edd56db47
pszczesz <pszczesz@poczta.onet.pl>
2018-11-13 09:39:07 +0100

    nowe walidacja1

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -5,13 +5,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace RazorPagesMovie.Models {
     public class Movie {
         public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3,ErrorMessage = "Długość pola 3 - 60 znaków")]
+        [Required(ErrorMessage = "Pole wymagane")]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date,ErrorMessage = "Podaj poprawną datę")]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
         [Column(TypeName = "decimal(10,2)")]
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 }
